4ca430c8ba04242cfcc60057c3d9240d3693660e
sergiolttz <sergiolattanzio05@gmail.com>
2025-12-03 15:08:28 -0400

    configuracion para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = '/portfolio/';
 export default defineConfig({
   plugins: [react()],
-})
+  base: repoName, 
+});
