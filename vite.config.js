e9af93d8c46213707a7226c5df40af05eb130d44
sergiolttz <sergiolattanzio05@gmail.com>
2025-12-03 15:32:18 -0400

    Revert "configuracion para github pages"
    This reverts commit 4ca430c8ba04242cfcc60057c3d9240d3693660e.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = '/portfolio/';
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: repoName, 
-});
+})
