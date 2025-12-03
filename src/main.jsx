3aeea42a9da017c07d7626ddd1b35812ee971921
sergiolttz <sergiolattanzio05@gmail.com>
2025-12-03 15:10:25 -0400

    configuracion para github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { LanguageProvider } from "./context/LanguageContext";
 import "./assets/styles/main.scss";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <LanguageProvider>
                 <App />
             </LanguageProvider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
