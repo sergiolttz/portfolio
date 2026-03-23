7edd02f6dd30840741924421647dddaccca43b4c
sergiolttz <sergiolattanzio05@gmail.com>
2026-03-23 13:36:06 -0400

    router router basename portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/portafolio">
+        <BrowserRouter basename="/portfolio">
             <LanguageProvider>
                 <App />
             </LanguageProvider>
